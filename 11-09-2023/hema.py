98efcfbb2e8ceb1273c2cd3980b29c3b783b27f6
hemalatha739582 <144675176+hemalatha739582@users.noreply.github.com>
2023-09-12 10:56:43 +0530

    Add files via upload

---
+++ 11-09-2023/hema.py
@@ -0,0 +1,12 @@
+Name=("Hemalatha Periyasamy")
+Address=("Puduppalaiyam rasipuram- 637408")
+Class=("Msc computer science")
+T=85
+E=83
+M=94
+Phy=91
+Che=88
+Bio=95
+Total=T+E+M+Phy+Che+Bio
+Avg=Total/6
+print("I'm",Name,"My address",Address,"i'm doing",Class,"My 12th mark:",T,E,M,Phy,Che,Bio,"My total mark",Total,"Avg",Avg)
